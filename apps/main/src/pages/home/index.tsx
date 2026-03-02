4783605fb889e1eb01d52b9b831ca3af45f1677f
zhangbo <zhangbo3.vendor@sensetime.com>
2026-03-02 15:16:22 +0800

    fix: 子应用修改

--- apps/main/src/pages/home/index.tsx
+++ apps/main/src/pages/home/index.tsx
@@ -1,13 +1,16 @@
+import { Flex } from "antd";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
   return (
     <div>
-      "父应用首页"
-      <button onClick={() => navigate("/agent")}>子应用1 Home</button>
-      <button onClick={() => navigate("/blog")}>子应用2 Home</button>
-      <button onClick={() => navigate("/login")}>去登录页</button>
+      父应用首页
+      <Flex gap={12}>
+        <button onClick={() => navigate("/agent")}>子应用1 Home</button>
+        <button onClick={() => navigate("/blog")}>子应用2 Home</button>
+        <button onClick={() => navigate("/login")}>去登录页</button>
+      </Flex>
     </div>
   );
 }
